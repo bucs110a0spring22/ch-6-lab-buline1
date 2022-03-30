5818fca3413ee8eb1f4e62d50e5c1f821d6a4ac4
Benjamin Uline J <98502703+buline1@users.noreply.github.com>
2022-03-30 01:36:54 +0000

    Finished Lab 6

--- README.md
+++ README.md
@@ -8,18 +8,20 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+This code takes the 3n+1 function provided and finds the number of iterations until the function output is 1 starting with the input for the upper bound given by the user. The second part of the code plots the start value to the iterations performed at that value from 1 to the upper bound value.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 1
 
-Grace days remaining: < # >/5
+Grace days remaining: 3/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+I believe all parts of the lab were completed. Had trouble with replit buffering so I was unable to fully check my code when submitting.
 
 ## REFERENCES:
- < List any outside resources used >
+- Class notes
+- https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.write
+- https://zetcode.com/python/add-string/#:~:text=Python%20add%20strings%20with%20%2B%20operator%20The%20easiest,programming%20we%20say%20that%20the%20operator%20is%20overloaded.
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+I am concerned the code will not run when getting graded. The code runs but the plot does not appear on the screen. If this is the case I would appreciate the option to re-submit. 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 
 def seq3np1(n):
     """
@@ -5,14 +6,52 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
+      if(n % 2) == 0:        # n is even
+          n = n // 2
+      else:                 # n is odd
+          n = n * 3 + 1
+      count += 1
+    return count              # the last print is 1
+print(seq3np1(3))
 
+def iterationGraph(myscreen=None, upper_limit=0):
+  turtle_write = turtle.Turtle()
+  turtle_graph = turtle.Turtle()
+  turtle_graph.goto(1,0)
+  turtle_graph.pendown()
+  turtle.speed(0)
+  myscreen.setworldcoordinates(0,0,10,10)
+
+  max_so_far = 0
+
+  start = 1
+  while(start <= upper_limit):
+    result = seq3np1(start)
+    if result > max_so_far:
+      max_so_far = result
+      turtle_write.goto(0, max_so_far)
+      turtle_write.clear()
+      turtle_write.write("Maximum so far:" + str(start) +  ", " + str(max_so_far))
+    myscreen.setworldcoordinates(0,0,start+10, max_so_far+10)
+    turtle_graph.goto(start, result)
+    start += 1
+  
 def main():
-	seq3np1(3)
+#PART A
+  upper_bound = int(input("\nPlease input a value for the upper bound of the range:  "))
+  if 0 > upper_bound:
+    print("Error")
+    exit()
+  else:
+    start = 1
+    while(start <= upper_bound):
+      print("Start:",start,"Iterations:",seq3np1(start))
+      start += 1
+#PART B
+  myscreen = turtle.Screen()
+  iterationGraph(myscreen, upper_bound)
+  myscreen.exitonclick()  
+  #seq3np1(3)
 main()
